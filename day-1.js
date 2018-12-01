af7c3321d74ec6faa12418e567b8d58c9026b449
Robert Sjoblom <robert.sjoblom@gmail.com>
2018-12-01 18:17:37 +0100

    Day 1 solution

---
+++ day-1.js
@@ -0,0 +1,21 @@
+// part 1
+eval(input.split('\n').join(' ')) // 416
+
+// part 2
+// this is a naive implementation, but my math isn't good enough
+// to create a better one
+const data = input.split('\n').map(x => Number(x))
+let freq = 0
+const seen = {}
+let a = true
+while(a) {
+  data.forEach(x => {
+    freq += x
+    if (seen[freq] != undefined) {
+      console.log(freq) // 56752
+      a = false
+    } else {
+      seen[freq] = 1
+    }
+  })
+}
